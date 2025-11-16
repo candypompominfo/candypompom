6eff7195483e483458f923735bc435e37e5799d6
candypompominfo <candycookieparty@gmail.com>
2025-11-17 01:55:00 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+// data.js
+const groupData = {
+  name: "candy♡pompom",
+  fandom: "POMMIE",
+  description: "ふわふわでキラキラな2人組アイドルユニット、candy♡pompom！",
+  debut: "2025.01.01",
+
+  members: [
+    {
+      name_jp: "真朱円ココア",
+      name_en: "Mashumaro Kokoa",
+      color: "赤",
+      role: "Red担当",
+      photo: "images/kokoa.jpg"
+    },
+    {
+      name_jp: "ひよ",
+      name_en: "Hiyo",
+      color: "白",
+      role: "White担当",
+      photo: "images/hiyo.jpg"
+    }
+  ],
+
+  sns: [
+    { label: "Twitter", handle: "@candy_pompom", url: "#" },
+    { label: "Instagram", handle: "@candypompom_official", url: "#" }
+  ]
+};
